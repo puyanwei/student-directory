d9d04eb24ec1d97fa0e0f8347393001f16f6c435
pyan83 <puyanwei@gmail.com>
2017-07-17 16:34:14 +0100

    Added number next to names in hash table

--- directory.rb
+++ directory.rb
@@ -1,5 +1,4 @@
 
-# List of all the students in hash table
 students = []
 
 def print_header
@@ -7,10 +6,9 @@ def print_header
   puts "-------------"
 end
 
-# Printing students in the hash
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each.with_index do |student, number|
+    puts "#{number + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -21,23 +19,16 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
+  students = []
   while !name.empty? do
-    # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
-  # return the array of students
   students
 end
 
-# Calling methods
 students = input_students
 print_header
 print(students)
