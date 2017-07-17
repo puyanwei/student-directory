8f9ac509d2ca852881842741b2d17974d97398ab
pyan83 <puyanwei@gmail.com>
2017-07-17 20:31:49 +0100

    8.3 Print names less then 12 characters

--- directory.rb
+++ directory.rb
@@ -5,10 +5,8 @@ def print_header
 end
 
 def print(students)
- puts "Type letter to search:"
- letter = gets.chomp.upcase
   students.each_with_index do |student, index|
-    if student[:name].start_with?(letter)
+    if student[:name].size < 12
      puts "#{index+1}. #{student[:name]} (#{student[:cohort]}) cohort"
     end
   end
