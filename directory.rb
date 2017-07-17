c3a252617bab4c843dcdd0cb3f8af0f6ae1fa091
pyan83 <puyanwei@gmail.com>
2017-07-17 18:59:35 +0100

    8.2 Only print names with letter starting

--- directory.rb
+++ directory.rb
@@ -1,14 +1,16 @@
 
-students = []
-
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
 def print(students)
-  students.each.with_index do |student, number|
-    puts "#{number + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+ puts "Type letter to search:"
+ letter = gets.chomp.upcase
+  students.each_with_index do |student, index|
+    if student[:name].start_with?(letter)
+     puts "#{index+1}. #{student[:name]} (#{student[:cohort]}) cohort"
+    end
   end
 end
 
@@ -19,12 +21,12 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = gets.chomp.capitalize
   students = []
   while !name.empty? do
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   students
 end
@@ -33,3 +35,18 @@ students = input_students
 print_header
 print(students)
 print_footer(students)
+
+=begin For testing
+students = [
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}]
+=end
