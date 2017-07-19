2a6667110545055cf42aa6e4668e8c165e9b11d6
pyan83 <puyanwei@gmail.com>
2017-07-19 12:45:05 +0100

    Refactorized code. Took out student parameters replaced with using @student

--- directory.rb
+++ directory.rb
@@ -1,24 +1,35 @@
+@students = []
 
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
-      puts "Invalid Choice. Try again."
-    end
+      puts "Invalid Selection. Try again."
   end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -26,19 +37,12 @@ def print_header
   puts "-------------".center(60)
 end
 
-def print(students)
-  if @n == 0
-     return
-  end
-
-  students.sort_by {|x| x[:cohort]}.select {|x| puts "#{x[:cohort]} - #{x[:name]}"}
+def print_student_list
+  @students.sort_by {|x| x[:cohort]}.select {|x| puts "#{x[:cohort]} - #{x[:name]}"}
 end
 
-def print_footer(names)
-  if @n == 0
-     return
-  puts "Overall, we have #{@n} great #{plural @n}".center(60)
-  end
+def print_footer
+  puts "Overall, we have #{@n} great #{plural}".center(60)
 end
 
 def input_students
@@ -61,30 +65,21 @@ def input_students
     end
     puts "Invalid month. Try again.".center(60)
   end
-
     puts "Hobby?".center(60)
     hobby = gets.gsub(/\n/,"").capitalize
     puts "Birthplace?".center(60)
     birthplace = gets.gsub(/\n/,"").capitalize
     puts "Height?".center(60)
     height = gets.gsub(/\n/,"").capitalize
-    students << {name: name, hobby: hobby, birthplace: birthplace, height: height, cohort: @cohort}
-    @n = students.count
-    puts "Now we have #{@n} #{plural @n}. Please input another.".center(60)
+    @students << {name: name, hobby: hobby, birthplace: birthplace, height: height, cohort: @cohort}
+    @n = @students.count
+    puts "Now we have #{@n} #{plural}. Please input another.".center(60)
     name = gets.gsub(/\n/,"").capitalize
   end
-  students
 end
 
-def plural(n)
- n == 1? "student" : "students"
+def plural
+ @n == 1? "student" : "students"
 end
 
-print_header
-<<<<<<< HEAD
 interactive_menu
-=======
-students = input_students
-print(students)
-print_footer(students)
->>>>>>> 8abdb26ffb846586e4970322362f35fe8279e6ca
